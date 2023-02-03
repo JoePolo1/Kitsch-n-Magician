6ffb2d3a3a82c6c37fcf2827d69d3568fdcae821
Tyler Brown <tyler.brown@telus.com>
2023-02-03 12:59:12 -0700

    sidebar start

--- kitsch-n-magician/src/App.js
+++ kitsch-n-magician/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <Navbar />
+    <Navbar />,
+    <Sidebar />
   );
 }
 
