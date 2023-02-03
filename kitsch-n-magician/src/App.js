b29835d663f6453fbff0c131524159abd7cdbac9
Hoss Agina <hoss.agina@telus.com>
2023-02-03 13:46:13 -0700

    sidebar functional

--- kitsch-n-magician/src/App.js
+++ kitsch-n-magician/src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
+import Sidebarleft from './components/Sidebarleft';
+
+
 
 function App() {
   return (
-    <Navbar />
+    <div>
+      <Navbar />
+      <Sidebarleft />
+    </div>
   );
 }
 
