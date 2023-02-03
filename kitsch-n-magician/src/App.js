26eff13cc4077cf5512f12801e9c1a3808493cad
Joe Polo <joe.polo@telus.com>
2023-02-03 12:46:39 -0800

    add sidebar

--- kitsch-n-magician/src/App.js
+++ kitsch-n-magician/src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
+import NewSideBar from './components/NewSideBar';
 
 function App() {
   return (
+    <div>
     <Navbar />
+    <NewSideBar />
+    </div>
   );
 }
 
---
+++ kitsch-n-magician/src/components/NewSideBar.jsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+
+export default function NewSideBar(props) {
+  
+  return (
+      <section className="siodebar">
+        <p>This is the SideBar</p>
+      </section>
+
+
+
+  )
+
+}
+
