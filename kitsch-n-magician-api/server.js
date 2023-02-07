4357e1e02d599689fcf6b0e341f7208b81b4000e
Tyler Brown <tyler.brown@telus.com>
2023-02-07 14:50:51 -0700

    update with login request working

--- kitsch-n-magician-api/server.js
+++ kitsch-n-magician-api/server.js
@@ -1,38 +1,47 @@
 // load .env data into process.env
 require('dotenv').config();
+
+const express = require('express');
 const cors = require('cors')
 
 // Web server config
 const sassMiddleware = require('./lib/sass-middleware');
-const express = require('express');
 const morgan = require('morgan');
+const bodyparser = require("body-parser");
 
-const PORT = process.env.PORT || 8080;
+// serve static files from ../build (needed for React)
+// const cwd = process.cwd();
+
+const PORT = 3001;
 const app = express();
 
-app.set('view engine', 'ejs');
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-app.use(morgan('dev'));
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  '/styles',
-  sassMiddleware({
-    source: __dirname + '/styles',
-    destination: __dirname + '/public/styles',
-    isSass: false, // false => scss, true => sass
-  })
-);
-app.use(express.static('public'));
-app.use(cors)
+// app.use(morgan('dev'));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(
+//   '/styles',
+//   sassMiddleware({
+//     source: __dirname + '/styles',
+//     destination: __dirname + '/public/styles',
+//     isSass: false, // false => scss, true => sass
+//   })
+// );
+// app.use(express.static('public'));
+app.use(cors())
+app.use(bodyparser.json());
+
+
 
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
 const usersRoutes = require('./routes/users');
+const db = require('./db/connection');
+
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
@@ -46,8 +55,9 @@ app.use('/users', usersRoutes);
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/test', (req, res) => {
+  console.log('hello sup')
+  res.json({test: 'hello'});
 });
 
 app.post('/login', (req, res) => {
@@ -55,7 +65,7 @@ app.post('/login', (req, res) => {
   const password = req.body.password;
 
   db.query(
-    "SELECT * FROM users WHERE username = ? AND password = ?",
+    "SELECT * FROM users WHERE username LIKE $1 AND password LIKE $2",
     [username, password],
     (err, result) => {
 
--- kitsch-n-magician/src/components/Login.jsx
+++ kitsch-n-magician/src/components/Login.jsx
@@ -35,10 +35,11 @@ export default function SignIn() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
+
   const [loginStatus, setLoginStatus] = useState("");
 
   const login = () => {
-    Axios.post("http://localhost:8080", {
+    Axios.post("http://localhost:3001/login", {
       username: username, 
       password: password,
     }).then((response) => {
@@ -74,8 +75,8 @@ export default function SignIn() {
           <Typography component="h1" variant="h5">
             Sign in
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
+          <Box component="form" noValidate sx={{ mt: 1 }}>
+            {/* <TextField
               margin="normal"
               required
               fullWidth
@@ -85,8 +86,15 @@ export default function SignIn() {
               autoComplete="email"
               autoFocus
               onChange={(event) => setUsername(event.target.value)}
-            />
-            <TextField
+              
+            /> */}
+            <input onChange={(event) => setUsername(event.target.value)}>
+
+</input>
+            <input onChange={(event) => setPassword(event.target.value)}>
+
+            </input>
+            {/* <TextField
               margin="normal"
               required
               fullWidth
@@ -95,21 +103,19 @@ export default function SignIn() {
               type="password"
               id="password"
               autoComplete="current-password"
-              onChange={(event) => setPassword(event.target.value)}
               
-            />
+              
+            /> */}
             <FormControlLabel
               control={<Checkbox value="remember" color="primary" />}
               label="Remember me"
               
             />
             <Button
-              type="submit"
               fullWidth
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
-              onClick={login}
-              
+              onClick={login}   
             >
               Sign In
             </Button>
