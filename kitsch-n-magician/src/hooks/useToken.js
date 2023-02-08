6c6c0d3d571f6ca11c443255bab7c29585939938
Joe Polo <joe.polo@telus.com>
2023-02-07 16:34:59 -0800

    login state fix

---
+++ kitsch-n-magician/src/hooks/useToken.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+
+export default function useToken() {
+
+
+  const getToken = () => {
+    const tokenString = localStorage.getItem('token') ;
+    const userToken = JSON.parse(tokenString);
+
+    return userToken;
+  }
+
+  const [token, setToken] = useState(getToken());
+
+  const saveToken = userToken => {if (typeof window !== 'undefined') {localStorage.setItem('token', JSON.stringify(userToken))
+  setToken(userToken.token)
+  }};
+
+  return {
+    setToken: saveToken,
+    getToken
+  }
+
+}
