b431fb4fc914844d2052ae6cb8d0cfd37f042384
Joe Polo <joe.polo@telus.com>
2023-02-09 19:17:37 -0800

    main page icon styling

--- kitsch-n-magician/src/components/FavouritesCard.jsx
+++ kitsch-n-magician/src/components/FavouritesCard.jsx
@@ -1,32 +1,16 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import { green, red } from '@mui/material/colors';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUtensils } from '@fortawesome/free-solid-svg-icons';
 import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
 import { Box, Divider, Paper } from '@mui/material';
 import VegetarianIcon from './Icons/Vegetarian';
-import NotVegetarianIcon from './Icons/NotVegetarian';
 import VeganIcon from './Icons/Vegan';
 import GlutenFreeIcon from './Icons/GlutenFree';
 import NotGlutenFreeIcon from './Icons/NotGlutenFree';
 import DairyFreeIcon from './Icons/DairyFree';
 import NotDairyFreeIcon from './Icons/NotDairyFree';
-import NotVegan from './Icons/NotVegan';
 
 
 const ExpandMore = styled((props) => {
@@ -129,11 +113,7 @@ export default function FavouritesCard(props) {
           
           </CardContent>
 
-      
-
-        
         </Box>
-
 
     </Paper>
       
