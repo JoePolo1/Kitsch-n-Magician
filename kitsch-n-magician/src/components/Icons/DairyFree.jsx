0c8519efe6ac09bcacbef0d1b762a5ad9aa6f73d
Joe Polo <joe.polo@telus.com>
2023-02-09 16:28:18 -0800

    final fix icons

--- kitsch-n-magician/src/components/Icons/DairyFree.jsx
+++ kitsch-n-magician/src/components/Icons/DairyFree.jsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCow } from '@fortawesome/free-solid-svg-icons';
 import Avatar from '@mui/material/Avatar';
+import { Box } from '@mui/system';
 
 
 export default function DairyFreeIcon() {
   return(
     <>
-      Dairy Free 
+    <Box sx={{
+            pr: "5px",
+            alignItems: "center",
+            display: 'flex'
+          }}>
+      Dairy Free </Box>
       <Avatar sx={{ bgcolor: "#154c79" }} >
         <FontAwesomeIcon icon={faCow} color="#96EB78" />
       </Avatar>
