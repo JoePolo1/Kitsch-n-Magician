24ba3d4b89dd755023324f2320400dd358851c7d
Joe Polo <joe.polo@telus.com>
2023-02-11 11:52:41 -0800

    restyle right column in matcher

--- kitsch-n-magician/src/components/MatchedColumn.jsx
+++ kitsch-n-magician/src/components/MatchedColumn.jsx
@@ -6,6 +6,8 @@ import ListItemText from '@mui/material/ListItemText';
 import { FixedSizeList } from 'react-window';
 import { Divider } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
 
 
 const drawerWidth = 240;
@@ -32,9 +34,26 @@ export default function MatchedColumn() {
       >
 
       </AppBar>
+
+      <Drawer
+        sx={{
+          display: 'flex',
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            mt: '4.3em',
+            width: drawerWidth,
+            boxSizing: 'border-box',
+            maxHeight: '100%'
+          }
+        }}
+        variant="permanent"
+        anchor="right"
+      >
+
+
     <Box
       sx={{ 
-        mt: '4.3em',
         width: '100%', 
         height: '100%', 
         maxWidth: 250, 
@@ -52,7 +71,7 @@ export default function MatchedColumn() {
             fontSize: '1.2em'}} bgcolor= '#18588c'>
             <header> Household Matches</header></Box>
         <Divider></Divider>
-      <FixedSizeList
+      {/* <FixedSizeList
         height={1020}
         width={250}
         itemSize={46}
@@ -60,8 +79,80 @@ export default function MatchedColumn() {
         overscanCount={5}
       >
         {renderRow}
-      </FixedSizeList>
+      </FixedSizeList> */}
+
+        <List>
+          <Drawer
+            sx={{
+              display: 'flex',
+
+              flexShrink: 0,
+              '& .MuiDrawer-paper': {
+                mt: '8em',
+
+                boxSizing: 'border-box',
+                maxHeight: '80%'
+              }
+            }}
+            variant="permanent"
+            anchor="right"
+          >
+          <Box sx={{ flexGrow: 0, 
+            width: 239, 
+            display: 'flex',
+            alignItems: 'left',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            fontSize: '1.2em'}} >
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Monday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Matched Recipe 1</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Tuesday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Matched Recipe 2</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Wednesday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Matched Recipe 3</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Thursday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Keep matching to fill!</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Friday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Keep matching to fill!</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Saturday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Keep matching to fill!</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'left', justifyContent: 'center', flexDirection: 'column', pl: '1em', bgcolor:'#D2E4F7'}}>
+                <p>Sunday:</p>
+              </Box>
+              <Box sx={{ flexGrow: 0, width: 239, display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
+                <p>Keep matching to fill!</p>
+              </Box>
+            </Box>
+          </Drawer>
+
+        </List>
+      
     </Box>
+    </Drawer>
     </>
   );
 }
