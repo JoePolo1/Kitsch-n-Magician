3b0342640face4f0008811a51a5ca059a5e8c8f7
Tyler Brown <tyler.brown@telus.com>
2023-02-13 19:20:14 -0700

    update debug fixes

--- kitsch-n-magician-api/db/seeds/07_favourite_recipes.sql
+++ kitsch-n-magician-api/db/seeds/07_favourite_recipes.sql
@@ -1,7 +1,7 @@
-INSERT INTO favourite_recipes(recipe_id, user_id)
-VALUES
-  (1, 1),
-  (1, 2),
-  (1, 3),
-  (2,1),
-  (2,2);
+-- INSERT INTO favourite_recipes(recipe_id, user_id)
+-- VALUES
+--   (1, 1),
+--   (1, 2),
+--   (1, 3),
+--   (2,1),
+--   (2,2);
