eeb48b075e5f41ef80eeca981d9f655b526462c0
Tyler Brown <tyler.brown@telus.com>
2023-02-15 18:01:06 -0700

    update with more font changes to matcher and sidebar left

--- kitsch-n-magician/src/components/IngredientListItem.jsx
+++ kitsch-n-magician/src/components/IngredientListItem.jsx
@@ -12,13 +12,24 @@ import { borderColor } from "@mui/system";
 // Renders individual ingredients on left side bar
 export default function IngredientListItem(props) {
   return (
-    <Box sx={{ width: "100%", maxWidth: 215, bgcolor: "#caf4da", borderRadius: '25px', mt: "0.25em", ml: "0.25em", mr: "0.25em", boxShadow: 6, borderBottom: '2px solid #4E5755', borderRight: '2px solid #4E5755'}}>
+    <Box sx={{ 
+      width: "100%", 
+      maxWidth: 215, 
+      bgcolor: "#caf4da", 
+      borderRadius: '25px', 
+      mt: "0.25em", 
+      ml: "0.25em", 
+      mr: "0.25em", 
+      boxShadow: 6, 
+      borderBottom: '2px solid #4E5755', 
+      borderRight: '2px solid #4E5755',
+      }}>
 
         <ListItemButton>
           <ListItemIcon sx={{ minWidth: 30 }}>
             <FontAwesomeIcon icon={faCube} />
           </ListItemIcon>
-          <ListItemText primary={props.name} />
+          <ListItemText disableTypography='True' primary={props.name} />
           <DeleteIngredientButton
             name={props.name}
             ingredients={props.ingredients}
